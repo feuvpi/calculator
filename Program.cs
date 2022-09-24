ac71f9e773a6d45761e48281e058c9677cb41ffa
feuvpi <fredvpg@gmail.com>
2022-09-24 17:37:50 -0300

    initial commit.

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿
+internal class Program
+{
+
+    static void sum()
+    {
+
+
+        Console.Clear();
+        Console.WriteLine("Primeiro valor:");
+        float v1 = float.Parse(Console.ReadLine());
+
+        Console.WriteLine("Segundo valor:");
+        float v2 = float.Parse(Console.ReadLine());
+
+        float resultado = v1 + v2;
+        Console.WriteLine("");
+        Console.WriteLine("O resultado da soma é: " + resultado);
+    }
+
+    static void Subtraction()
+    {
+        Console.Clear();
+        Console.WriteLine("First value:");
+        float v1 = float.Parse(Console.ReadLine());
+
+        Console.WriteLine("Second value:");
+        float v2 = float.Parse(Console.ReadLine());
+
+        Console.WriteLine("");
+        Console.WriteLine($"Result is: {v1 - v2}");
+        Console.ReadKey();
+
+    }
+
+
+    private static void Main(string[] args)
+    {
+
+        Subtraction();//functions
+
+
+    }
+
+
+}
